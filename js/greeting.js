d9586059c20630ffbf55983e37b586e45f2ea1c3
David Verna <vernadavidn@gmail.com>
2023-04-06 13:41:33 -0300

    Enhanced code reading

--- js/greeting.js
+++ js/greeting.js
@@ -1,17 +1,18 @@
 const userName = prompt('Hi!! What\'s your name?');
 
-// If the user puts the name completely in lower or upper case letters
-const capitalizedUserName = userName.split(' ').map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()).join(' ');
+let greeting = `Hi!`;
 
-let greeting = `Hi, ${capitalizedUserName}!`;
-
-if (userName === null || userName === '') {
-    greeting = 'Hi!';
+if (userName) {
+    // If the user puts the name completely in lower or upper case letters
+    const capitalizedUserName = userName.split(' ')
+    .map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
+    .join(' ');
+    greeting = `Hi, ${capitalizedUserName}!`
 }
 
 // Inserting the greeting in index.html
 const insertingGreetingFather = document.querySelector('.greeting');
-const InsertingGreetingChild = document.createElement('H2');
-InsertingGreetingChild.classList.add('centered-text');
-InsertingGreetingChild.textContent = greeting;
-insertingGreetingFather.appendChild(InsertingGreetingChild);
+const insertingGreetingChild = document.createElement('h2');
+insertingGreetingChild.classList.add('centered-text');
+insertingGreetingChild.textContent = greeting;
+insertingGreetingFather.appendChild(insertingGreetingChild);
